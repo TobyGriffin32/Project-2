ec62daead1ecd52bd61d9b700664984eda8ce193
Toby Griffin <Toby Griffin@DESKTOP-BVA1HMU.attlocal.net>
2020-04-11 19:50:02 -0500

    CellState with added methods

--- Project2/src/CellState.java
+++ Project2/src/CellState.java
@@ -1,4 +1,23 @@
+import java.util.Map;
 
 public enum CellState {
+	
+	OFF(';'), 
+	ON('0');
+	
+	private char symbol;
+	private static Map<Character, CellState> SYMBOL_TO_STATE;
+	
+	private CellState(char symbol) {
+		this.symbol = symbol;
+	}
+	
+	public static CellState getState(char symbol) {
+		return SYMBOL_TO_STATE.get(symbol);
+	}
+	
+	public String toString() {
+		return "" + getState(symbol);
+	}
 
 }
