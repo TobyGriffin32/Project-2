c2b8599e021178663bddc0e004e458400793556a
Toby Griffin <Toby Griffin@DESKTOP-BVA1HMU.attlocal.net>
2020-04-12 22:52:35 -0500

    Implemented abstract Rule class

---
+++ Project2/src/Rule.java
@@ -0,0 +1,31 @@
+
+public abstract class Rule {
+	
+	private int ruleNum;
+	
+	protected Rule(int ruleNum) {
+		this.ruleNum = ruleNum;
+	}
+	
+	public int getRuleNum() {
+		return ruleNum;
+	}
+	
+	public Generation evolve(Generation gen, BoundaryConditions bs) {
+		EvolvedCell[] newCells = new EvolvedCell[gen.size()];
+		for(int i = 0; i < newCells.length; ++i) {
+			newCells[i] = evolve(getNeighborhood(i, gen, bs));
+		}
+		Generation newGen = new Generation(newCells);
+		return newGen;
+	}
+	
+	public abstract int getNumSubrules();
+	
+	public abstract Cell[] getNeighborhood(int cellIdx, Generation gen, BoundaryConditions bc);
+	
+	public abstract EvolvedCell evolve(Cell[] neighborhood);
+	
+	public abstract String toString();
+
+}
